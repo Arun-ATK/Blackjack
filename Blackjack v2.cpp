432c5a3ee71f7d9ef33106d8d56be1622e3810f1
Arun-ATK <61749361+Arun-ATK@users.noreply.github.com>
2020-07-08 21:24:41 +0530

    Added files

---
+++ Blackjack v2.cpp
@@ -0,0 +1,253 @@
+// Blackjack v2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// A Simple Blackjack game created by Arun TK.
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include "Enums.h"
+//#include "Card.h"
+//#include "Deck.h"
+
+int randomNumberGenerator(int, int);
+
+class Card
+{
+private:
+    Rank m_rank{};
+    Suit m_suit{};
+
+public:
+    Card(Rank rank = Rank::RANK_2, Suit suit = Suit::SPADES) : m_rank{ rank }, m_suit{ suit }
+    {}
+
+    void print() const
+    {
+        switch (m_rank) {
+        case Rank::RANK_2: std::cout << 2;
+            break;
+        case Rank::RANK_3: std::cout << 3;
+            break;
+        case Rank::RANK_4: std::cout << 4;
+            break;
+        case Rank::RANK_5: std::cout << 5;
+            break;
+        case Rank::RANK_6: std::cout << 6;
+            break;
+        case Rank::RANK_7: std::cout << 7;
+            break;
+        case Rank::RANK_8: std::cout << 8;
+            break;
+        case Rank::RANK_9: std::cout << 9;
+            break;
+        case Rank::RANK_10: std::cout << 10;
+            break;
+        case Rank::RANK_JACK: std::cout << 'J';
+            break;
+        case Rank::RANK_QUEEN: std::cout << 'Q';
+            break;
+        case Rank::RANK_KING: std::cout << 'K';
+            break;
+        case Rank::RANK_ACE: std::cout << 'A';
+            break;
+        default: std::cout << '?';
+        }
+
+        switch (m_suit) {
+        case Suit::SPADES: std::cout << 's';
+            break;
+        case Suit::DIAMONDS: std::cout << 'd';
+            break;
+        case Suit::CLUBS: std::cout << 'c';
+            break;
+        case Suit::HEARTS: std::cout << 'h';
+            break;
+        }
+    }
+
+    int value() const
+    {
+        if (m_rank <= Rank::RANK_10) {
+            return (static_cast<int>(m_rank) + 2);
+        }
+        else {
+            switch (m_rank) {
+            case Rank::RANK_JACK:
+            case Rank::RANK_QUEEN:
+            case Rank::RANK_KING:
+                return 10;
+            case Rank::RANK_ACE: return 11;
+
+            default: return 666;
+
+            }
+        }
+    }
+};
+
+class Deck
+{
+private:
+    std::vector<Card> m_hand{};
+    int m_points{};
+
+public:
+    Deck(Members a)
+    {
+        addCard();
+        if (a == Members::Player) {
+            addCard();
+        }
+    }
+
+    void addCard()
+    {
+        //Card a{ Rank::RANK_10, Suit::CLUBS };
+        auto rank{ static_cast<Rank>(randomNumberGenerator(0, static_cast<int>(Rank::MAX_RANK) - 1)) };
+        auto suit{ static_cast<Suit>(randomNumberGenerator(0, static_cast<int>(Suit::MAX_SUIT) - 1)) };
+
+        m_hand.push_back({ rank, suit });
+    }
+
+    int getPoints()
+    {
+        int sum{};
+        for (auto i : m_hand) {
+            sum += i.value();
+        }
+        m_points = sum;
+        return sum;
+    }
+
+    void showHand() const
+    {
+        for (auto i : m_hand) {
+            i.print();
+            std::cout << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    bool crossed21()
+    {
+        if (m_points > 21) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool higherThan(Deck& a)
+    {
+        if (m_points >= a.m_points) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+};
+
+int randomNumberGenerator(int min, int max)
+{
+    static constexpr double fraction{ 1.0 / (RAND_MAX + 1.0) };
+    return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
+}
+
+bool chooseOne()
+{
+    std::string a;
+    while (true) {
+        std::cin >> a;
+        if (a == "y") {
+            return true;
+        }
+        else if (a == "n") {
+            return false;
+        }
+        else {
+            std::cout << "Invalid input! Please choose again: ";
+        }
+    }
+}
+
+void startScreen()
+{
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << std::endl;
+    std::cout << "      Welcome to Blackjack!      " << std::endl;
+    std::cout << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+}
+
+int main()
+{
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    while (true) {
+        system("cls");
+        startScreen();
+
+        Deck dealer{ Members::Dealer };
+        Deck player{ Members::Player };
+
+        while (true) {
+            std::cout << "Dealer: ";
+            dealer.showHand();
+            std::cout << "Dealer points: " << dealer.getPoints() << std::endl << std::endl;
+
+            std::cout << "Player: ";
+            player.showHand();
+            std::cout << "Your points: " << player.getPoints() << std::endl;
+
+            if (player.crossed21()) {
+                std::cout << "You've exceeded 21! Game lost... \n";
+                break;
+            }
+
+            std::cout << "Do you want another card (y/n): ";
+            if (chooseOne()) {
+                player.addCard();
+                continue;
+            }
+            else {
+                std::cout << "Your final score: " << player.getPoints() << std::endl;
+                std::cout << "Dealer's turn...\n";
+
+                while (!dealer.crossed21()) {
+                    if (!dealer.higherThan(player)) {
+                        dealer.addCard();
+                        dealer.getPoints();
+                        continue;
+                    }
+                    else {
+                        std::cout << "Dealer got " << dealer.getPoints() << std::endl;
+                        std::cout << "You lose...\n";
+                        break;
+                    }
+                }
+                if(dealer.crossed21()) {
+                    std::cout << "Dealer crossed 21! " << dealer.getPoints() << std::endl;
+                    std::cout << "You win!\n";
+                }
+                break;
+            }
+        }
+
+        std::cout << "Do you want to play again (y/n): ";
+        if (!chooseOne()) {
+            break;
+        }
+    }
+
+    std::cout << "Press enter to exit.";
+    //std::cin.ignore(1000, '\n');
+    std::string exiter;
+    std::cin >> exiter;
+
+    return 0;
+}
+
---
+++ README.txt
@@ -0,0 +1,16 @@
+Objective:
+Beat the dealer by getting the count closest to 21, without going over 21.
+
+Card Value:
+Each card has a point equal to its number. Jack, Queen, and King has a value of 10. Ace has 11 points.
+
+Gameplay:
+At the start of the game, you are dealt two cards while the dealer takes one card. You are given two options,
+either to get another card, or pass on the turn to the dealer.
+
+If you feel you can get another card without going over 21, you may choose to get another card, after which you 
+are presented the option again.
+
+If you don't want another card, you can pass on the turn to the dealer. You will not be able to get cards again,
+so choose wisely. If the dealer has exceeded 21 in his turn, you win the game. If he gets a count higher than
+yours, but lower than 21, you lose the game.
